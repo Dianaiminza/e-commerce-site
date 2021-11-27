c823709590b04030b551d7545f1a998620f13704
Dianaiminza <dianaiminza.99@gmail.com>
2021-11-27 14:39:00 +0300

    Updates

---
+++ precache-manifest.4891c9796bb544238c4d1b2039921d03.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a72aa5576bbad155f663dda7db666a",
+    "url": "/e-commerce-site/index.html"
+  },
+  {
+    "revision": "4728a5dbce2e57bbcf7b",
+    "url": "/e-commerce-site/static/css/main.44e4e416.chunk.css"
+  },
+  {
+    "revision": "39c387f533b4a5ecf64d",
+    "url": "/e-commerce-site/static/js/2.353a65ce.chunk.js"
+  },
+  {
+    "revision": "4728a5dbce2e57bbcf7b",
+    "url": "/e-commerce-site/static/js/main.77737340.chunk.js"
+  },
+  {
+    "revision": "3e818d609eba1e8df0eb",
+    "url": "/e-commerce-site/static/js/runtime~main.1ff4588e.js"
+  },
+  {
+    "revision": "a7c95f0815c89174cb5212f03187f36e",
+    "url": "/e-commerce-site/static/media/cottoncordel1.a7c95f08.jpg"
+  },
+  {
+    "revision": "e4139efe9d30f4c30a2e64eac484b219",
+    "url": "/e-commerce-site/static/media/cottoncordel10.e4139efe.jpg"
+  },
+  {
+    "revision": "e4139efe9d30f4c30a2e64eac484b219",
+    "url": "/e-commerce-site/static/media/cottoncordel11.e4139efe.jpg"
+  },
+  {
+    "revision": "46ede767e44b0b772edfc66a6c7f0de5",
+    "url": "/e-commerce-site/static/media/cottoncordel16.46ede767.jpg"
+  },
+  {
+    "revision": "46ede767e44b0b772edfc66a6c7f0de5",
+    "url": "/e-commerce-site/static/media/cottoncordel17.46ede767.jpg"
+  },
+  {
+    "revision": "a7c95f0815c89174cb5212f03187f36e",
+    "url": "/e-commerce-site/static/media/cottoncordel2.a7c95f08.jpg"
+  },
+  {
+    "revision": "32382a1c0c85cad7b280028416547fa6",
+    "url": "/e-commerce-site/static/media/cottoncordel20.32382a1c.jpg"
+  },
+  {
+    "revision": "5c909fe00fe98a0a858b4be3251c4cb9",
+    "url": "/e-commerce-site/static/media/cottoncordel3.5c909fe0.jpg"
+  },
+  {
+    "revision": "5c909fe00fe98a0a858b4be3251c4cb9",
+    "url": "/e-commerce-site/static/media/cottoncordel4.5c909fe0.jpg"
+  },
+  {
+    "revision": "c3771fdc258cfb4d9fbedfb1f4b14118",
+    "url": "/e-commerce-site/static/media/cottoncordel5.c3771fdc.jpg"
+  },
+  {
+    "revision": "c3771fdc258cfb4d9fbedfb1f4b14118",
+    "url": "/e-commerce-site/static/media/cottoncordel6.c3771fdc.jpg"
+  },
+  {
+    "revision": "9872a9bcf0b8c62b762bad829e86dafd",
+    "url": "/e-commerce-site/static/media/cottoncordel7.9872a9bc.jpg"
+  },
+  {
+    "revision": "9872a9bcf0b8c62b762bad829e86dafd",
+    "url": "/e-commerce-site/static/media/cottoncordel8.9872a9bc.jpg"
+  },
+  {
+    "revision": "22961869e6477422aac6f0776b76743c",
+    "url": "/e-commerce-site/static/media/download.22961869.jpg"
+  },
+  {
+    "revision": "58e05f2807a58bbb1455116b96e6057f",
+    "url": "/e-commerce-site/static/media/heddon1.58e05f28.jpg"
+  },
+  {
+    "revision": "20f62ce1ac54e09db2c9e41e2caf966c",
+    "url": "/e-commerce-site/static/media/heddon11.20f62ce1.jpg"
+  },
+  {
+    "revision": "20f62ce1ac54e09db2c9e41e2caf966c",
+    "url": "/e-commerce-site/static/media/heddon12.20f62ce1.jpg"
+  },
+  {
+    "revision": "e3b816fcbe345695ab498daa0c81089a",
+    "url": "/e-commerce-site/static/media/heddon15.e3b816fc.jpg"
+  },
+  {
+    "revision": "e3b816fcbe345695ab498daa0c81089a",
+    "url": "/e-commerce-site/static/media/heddon16.e3b816fc.jpg"
+  },
+  {
+    "revision": "e11925457ce3de3c136b65129ed231dc",
+    "url": "/e-commerce-site/static/media/heddon2.e1192545.jpg"
+  },
+  {
+    "revision": "32382a1c0c85cad7b280028416547fa6",
+    "url": "/e-commerce-site/static/media/heddon3.32382a1c.jpg"
+  },
+  {
+    "revision": "546792c21d7983e37f926d2b80938375",
+    "url": "/e-commerce-site/static/media/heddon5.546792c2.jpg"
+  },
+  {
+    "revision": "58e05f2807a58bbb1455116b96e6057f",
+    "url": "/e-commerce-site/static/media/heddon6.58e05f28.jpg"
+  },
+  {
+    "revision": "e11925457ce3de3c136b65129ed231dc",
+    "url": "/e-commerce-site/static/media/heddon7.e1192545.jpg"
+  },
+  {
+    "revision": "546792c21d7983e37f926d2b80938375",
+    "url": "/e-commerce-site/static/media/heddon9.546792c2.jpg"
+  },
+  {
+    "revision": "ef9408726a01b3660fa27452ab6c1cd1",
+    "url": "/e-commerce-site/static/media/mepps1.ef940872.jpg"
+  },
+  {
+    "revision": "e83a629a205558e363baa5c1b3670e9b",
+    "url": "/e-commerce-site/static/media/mepps10.e83a629a.jpg"
+  },
+  {
+    "revision": "b9afda437ceaa2175f3c7613c9f19446",
+    "url": "/e-commerce-site/static/media/mepps14.b9afda43.jpg"
+  },
+  {
+    "revision": "b9afda437ceaa2175f3c7613c9f19446",
+    "url": "/e-commerce-site/static/media/mepps15.b9afda43.jpg"
+  },
+  {
+    "revision": "c8e4b3ce6c284d60dfb981b20550ff61",
+    "url": "/e-commerce-site/static/media/mepps17.c8e4b3ce.jpg"
+  },
+  {
+    "revision": "c8e4b3ce6c284d60dfb981b20550ff61",
+    "url": "/e-commerce-site/static/media/mepps18.c8e4b3ce.jpg"
+  },
+  {
+    "revision": "ef9408726a01b3660fa27452ab6c1cd1",
+    "url": "/e-commerce-site/static/media/mepps2.ef940872.jpg"
+  },
+  {
+    "revision": "e8746360ff79fe91cb04385e34a20489",
+    "url": "/e-commerce-site/static/media/mepps3.e8746360.jpg"
+  },
+  {
+    "revision": "e8746360ff79fe91cb04385e34a20489",
+    "url": "/e-commerce-site/static/media/mepps4.e8746360.jpg"
+  },
+  {
+    "revision": "826bccdb5a957d0d28a59213bf06a39f",
+    "url": "/e-commerce-site/static/media/mepps6.826bccdb.jpg"
+  },
+  {
+    "revision": "826bccdb5a957d0d28a59213bf06a39f",
+    "url": "/e-commerce-site/static/media/mepps7.826bccdb.jpg"
+  },
+  {
+    "revision": "e83a629a205558e363baa5c1b3670e9b",
+    "url": "/e-commerce-site/static/media/mepps9.e83a629a.jpg"
+  },
+  {
+    "revision": "ccc37240890d78b85d69fa5745523d3f",
+    "url": "/e-commerce-site/static/media/rapala10.ccc37240.jpg"
+  },
+  {
+    "revision": "ccc37240890d78b85d69fa5745523d3f",
+    "url": "/e-commerce-site/static/media/rapala11.ccc37240.jpg"
+  },
+  {
+    "revision": "7181dec7edd9e0746b303b871bef7610",
+    "url": "/e-commerce-site/static/media/rapala12.7181dec7.jpg"
+  },
+  {
+    "revision": "7181dec7edd9e0746b303b871bef7610",
+    "url": "/e-commerce-site/static/media/rapala13.7181dec7.jpg"
+  },
+  {
+    "revision": "58aa04534dcf6744156fb74b5a3e04ea",
+    "url": "/e-commerce-site/static/media/rapala14.58aa0453.jpg"
+  },
+  {
+    "revision": "58aa04534dcf6744156fb74b5a3e04ea",
+    "url": "/e-commerce-site/static/media/rapala15.58aa0453.jpg"
+  },
+  {
+    "revision": "d6522223256ee1769988c63eb872f2fd",
+    "url": "/e-commerce-site/static/media/rapala16.d6522223.jpg"
+  },
+  {
+    "revision": "8a5331500b1028bad2fcf7b1ed1afdad",
+    "url": "/e-commerce-site/static/media/rapala3.8a533150.jpg"
+  },
+  {
+    "revision": "8a5331500b1028bad2fcf7b1ed1afdad",
+    "url": "/e-commerce-site/static/media/rapala4.8a533150.jpg"
+  },
+  {
+    "revision": "7201124a3e7ffa43a9f9ae75c1f89472",
+    "url": "/e-commerce-site/static/media/rapala5.7201124a.jpg"
+  },
+  {
+    "revision": "7201124a3e7ffa43a9f9ae75c1f89472",
+    "url": "/e-commerce-site/static/media/rapala6.7201124a.jpg"
+  },
+  {
+    "revision": "fe81471e1e8310d29d78ee88c2a8d313",
+    "url": "/e-commerce-site/static/media/rebel11.fe81471e.jpg"
+  },
+  {
+    "revision": "88d2923dac35ab7c3e5cf9213a155134",
+    "url": "/e-commerce-site/static/media/rebel18.88d2923d.jpg"
+  }
+]);
